f62e73268e9cb39f3800aa8abe19668468ca93e5
tezcansarizeybek <sarizeybektezcan@gmail.com>
2022-05-06 16:52:09 +0300

    ui checkpoint

--- lib/model/comment.dart
+++ lib/model/comment.dart
@@ -4,11 +4,13 @@ class Comment {
     String? comment,
     String? motive,
     String? local,
+    String? url,
   }) {
     _uuid = uuid;
     _comment = comment;
     _motive = motive;
     _local = local;
+    _url = url;
   }
 
   Comment.fromJson(dynamic json) {
@@ -16,27 +18,32 @@ class Comment {
     _comment = json['comment'];
     _motive = json['motive'];
     _local = json['local'];
+    _url = json['url'];
   }
   String? _uuid;
   String? _comment;
   String? _motive;
   String? _local;
+  String? _url;
   Comment copyWith({
     String? uuid,
     String? comment,
     String? motive,
     String? local,
+    String? url,
   }) =>
       Comment(
         uuid: uuid ?? _uuid,
         comment: comment ?? _comment,
         motive: motive ?? _motive,
         local: local ?? _local,
+        url: url ?? _url,
       );
   String? get uuid => _uuid;
   String? get comment => _comment;
   String? get motive => _motive;
   String? get local => _local;
+  String? get url => _url;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
@@ -44,6 +51,7 @@ class Comment {
     map['comment'] = _comment;
     map['motive'] = _motive;
     map['local'] = _local;
+    map['url'] = _url;
     return map;
   }
 }
--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:motivapp/model/motive.dart';
-import 'package:motivapp/view/admin_login_page.dart';
-import 'package:motivapp/view/comment_page.dart';
 import 'package:motivapp/view_model/motive_vm.dart';
 
 class MainPage extends StatelessWidget {
@@ -13,76 +9,133 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Get.put(MotiveVM());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(""),
-        actions: [
-          InkWell(
-            child: SizedBox(
-              width: 50,
-            ),
-            onDoubleTap: () {
-              Get.to(() => const AdminLoginPage());
-            },
+        // appBar: AppBar(
+        //   title: const Text(""),
+        //   actions: [
+        //     InkWell(
+        //       child: const SizedBox(
+        //         width: 50,
+        //       ),
+        //       onDoubleTap: () {
+        //         Get.to(() => const AdminLoginPage());
+        //       },
+        //     ),
+        //     IconButton(
+        //         onPressed: () => Get.find<MotiveVM>().getLangs(), icon: const Icon(Icons.language))
+        //   ],
+        // ),
+        body: GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.red.shade200, Colors.pinkAccent.shade100])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Text("D'mottie",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, color: Colors.white)),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Icon(Icons.star),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text("Merhaba\nBen Mottie\nSenin adın ne?",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30, color: Colors.white)),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 18, right: Get.size.width / 4, left: Get.size.width / 4),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
+                          fillColor: Colors.white24,
+                          filled: true,
+                        ),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.white, decoration: TextDecoration.none),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          IconButton(
-              onPressed: () => Get.find<MotiveVM>().getLangs(), icon: const Icon(Icons.language))
-        ],
+        ),
       ),
-      body: GetBuilder<MotiveVM>(
-        initState: (_) async {
-          Get.put(MotiveVM());
-        },
-        builder: (logic) {
-          return Obx(() {
-            return StreamBuilder<QuerySnapshot<Motive>>(
-                stream: FirebaseFirestore.instance
-                    .collection('motives')
-                    .where("local", isEqualTo: logic.local.value)
-                    .withConverter<Motive>(
-                      fromFirestore: (snapshots, _) => Motive.fromJson(snapshots.data()!),
-                      toFirestore: (motive, _) => motive.toJson(),
-                    )
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final doc = snapshot.requireData;
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 131),
-                      itemBuilder: (context, index) {
-                        var data = doc.docs[index].data();
-                        return Container(
-                          padding: const EdgeInsets.all(8),
-                          child: InkWell(
-                            onTap: () async {
-                              await logic.getCommentByMotive(data.category ?? "");
-                              if (logic.comment.value.comment != null) {
-                                Get.to(() => const CommentPage());
-                              }
-                            },
-                            child: Stack(
-                              children: [
-                                Image.network(data.icon ?? ""),
-                                Positioned(
-                                  child: Text("${data.category}", textAlign: TextAlign.center),
-                                  bottom: 0,
-                                  left: 0,
-                                  right: 0,
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      itemCount: doc.size,
-                    );
-                  } else {
-                    return Container();
-                  }
-                });
-          });
-        },
-      ),
-    );
+    )
+        // GetBuilder<MotiveVM>(
+        //   initState: (_) async {
+        //     Get.put(MotiveVM());
+        //   },
+        //   builder: (logic) {
+        //     return Obx(() {
+        //       return StreamBuilder<QuerySnapshot<Motive>>(
+        //           stream: FirebaseFirestore.instance
+        //               .collection('motives')
+        //               .where("local", isEqualTo: logic.local.value)
+        //               .withConverter<Motive>(
+        //                 fromFirestore: (snapshots, _) => Motive.fromJson(snapshots.data()!),
+        //                 toFirestore: (motive, _) => motive.toJson(),
+        //               )
+        //               .snapshots(),
+        //           builder: (context, snapshot) {
+        //             if (snapshot.hasData) {
+        //               final doc = snapshot.requireData;
+        //               return GridView.builder(
+        //                 gridDelegate:
+        //                     const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 131),
+        //                 itemBuilder: (context, index) {
+        //                   var data = doc.docs[index].data();
+        //                   return Container(
+        //                     padding: const EdgeInsets.all(8),
+        //                     child: InkWell(
+        //                       onTap: () async {
+        //                         await logic.getCommentByMotive(data.category ?? "");
+        //                         if (logic.comment.value.comment != null) {
+        //                           Get.to(() => const CommentPage());
+        //                         }
+        //                       },
+        //                       child: Stack(
+        //                         children: [
+        //                           Image.network(data.icon ?? ""),
+        //                           Positioned(
+        //                             child: Text("${data.category}", textAlign: TextAlign.center),
+        //                             bottom: 0,
+        //                             left: 0,
+        //                             right: 0,
+        //                           )
+        //                         ],
+        //                       ),
+        //                     ),
+        //                   );
+        //                 },
+        //                 itemCount: doc.size,
+        //               );
+        //             } else {
+        //               return Container();
+        //             }
+        //           });
+        //     });
+        //   },
+        // ),
+        );
   }
 }
